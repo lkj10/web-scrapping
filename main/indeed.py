d93827c89110d32ee037129265b4166b47d035c6
lkj10 <lkj10@users.noreply.github.com>
2021-04-02 13:51:25 +0900

    added indeed.py

---
+++ main/indeed.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  2 12:03:21 2021
+
+@author: LEEKWANGJIN
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+
+URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
+
+def extract_indeed_pages():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class" : "pagination"})
+    
+    links = pagination.find_all('a')
+    pages = []
+    
+    for link in links[:-1]:
+        pages.append(int(link.string))
+        
+    max_page = pages[-1]
+    return max_page
+
+def extract_indeed_jobs(last_page):
+    for page in range(last_page):
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        print(result.status_code)
--- main/main.py
+++ main/main.py
@@ -1,29 +1,6 @@
-import requests
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-answer = 'n'
 
-while True:
-    if answer == 'y':
-        break
-    elif answer == 'n':
-        print("Welcome to IsItDown.py!")
-        print("Please write a URLs you want to check. (separated by comma)")
-        b = input()
-        for x in b.split(','):
-            x = x.strip()
-            if x[-4:] != '.com':
-                print(f"{x} is not a vaild URL.")
-            else:
-                if x[0:7] != 'http://':
-                    x = 'http://' + x
-                try:
-                    url = requests.get(f'{x}')
-                    if(url.status_code == 200):
-                        print(f"{x} is up!")
-                    else:
-                        print(f"{x} is down!")
-                except:
-                    print(f"{x} is down!")
-        print("Do you want to start over? y/n")
-        answer = input()
-        continue
+last_indeed_jobs = extract_indeed_pages()
+
+extract_indeed_jobs(last_indeed_jobs)
