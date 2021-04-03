0986e20b440595a95f3063dede2a46c871283bf7
lkj10 <lkj10@users.noreply.github.com>
2021-04-03 17:39:19 +0900

    Extracted Locations and Finish up

--- main/main.py
+++ main/main.py
@@ -3,4 +3,6 @@ from indeed import extract_indeed_pages, extract_indeed_jobs
 
 last_indeed_jobs = extract_indeed_pages()
 
-extract_indeed_jobs(last_indeed_jobs)
+indeed_jobs = extract_indeed_jobs(last_indeed_jobs)
+
+print(indeed_jobs)
