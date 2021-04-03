3918c4679ba83a2e2b512bb0274018e1bd0ac127
lkj10 <lkj10@users.noreply.github.com>
2021-04-03 17:53:39 +0900

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,5 +1,8 @@
 from indeed import get_jobs as get_indeed_jobs
+from stackoverflow import get_jobs as get_so_jobs
 
-indeed_jobs = get_indeed_jobs()
+#indeed_jobs = get_indeed_jobs()
 
-print(indeed_jobs)
+so_jobs = get_so_jobs()
+
+#print(indeed_jobs)
